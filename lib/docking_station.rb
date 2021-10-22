6979ac89ee3a49139260eda1f7961e17e8e23234
willjshark <willjshark@gmial.com>
2021-10-22 10:44:03 +0100

    challenge 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,23 @@
 # require './lib/bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock_bike(bike)
-   bike
+  
+  fail 'The docking station can only hold 1 bike' if @bike
+  @bike = bike
   end
+
+  #def bike
+    #@bike
+  #end
 end
 
 #Challenge 11 step 4. Read up on instance variables and attr_reader.
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,19 +5,45 @@ describe DockingStation do
   #   it 'should respond to method release_bike' do
   #     expect(subject).to respond_to(:release_bike)
   #   end
+  
+
+  describe '#release_bike' do
   it { should respond_to(:release_bike) }
-
-  it 'release_bike should return bike object' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  
+  it 'release_bike should raise error if there is no bike' do
+    #bike = Bike.new
+    #subject.dock_bike(bike)
+    expect{ subject.release_bike }.to raise_error 'No bikes available'
   end
-
+end
+ 
+  describe '#dock_bike' do
   it 'should respond to method dock_bike' do
     expect(subject).to respond_to (:dock_bike)
   end
 
   it 'method dock_bike should store bike object' do
     bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq (bike)
+    expect(subject.dock_bike(bike)).to eq bike
   end
+  
+  it { is_expected.to respond_to (:bike) }
+
+  end
+
+  it 'Should throw error if at max capacity' do
+  bike = Bike.new
+  subject.dock_bike(bike)
+  expect{subject.dock_bike(bike)}.to raise_error 'The docking station can only hold 1 bike'
+  end
+  
+  
+  it 'checks if bike is available' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+
+
 end
+
